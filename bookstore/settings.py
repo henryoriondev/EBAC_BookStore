ba6a36590bbb16e94d0ddb9797c4d881e913e1ea
henryoriondev <henryorion.dev@gmail.com>
2026-06-13 01:52:58 -0300

    feat: add docker compose com postgre

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'bookstore'),
+        'USER': os.getenv('DB_USER', 'bookstore'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'bookstore123'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
